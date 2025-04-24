bd7abffa594494e80a046fd2a4e5d6a3b5a11e6f
Zen <michaela.salamero@gmail.com>
2025-04-25 00:16:05 +0800

    Updates

---
+++ assets/AboutView-Crhz_4xq.js
@@ -0,0 +1,1 @@
+import{_ as o,c as s,a as t,o as a}from"./index-BoW5PdiM.js";const r={},i={class:"about"};function n(c,e){return a(),s("div",i,e[0]||(e[0]=[t("h1",null,"This is an about page",-1)]))}const u=o(r,[["render",n],["__file","C:/workdir/zensalamero.github.io/src/views/AboutView.vue"]]);export{u as default};
