c44fc53c3b6365922dee67d32295fdcc0f2cdf3f
Zen <michaela.salamero@gmail.com>
2025-04-24 18:59:18 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'jsx-a11y'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off', // Not needed in React 17+
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
