414159aab9e615231fadc49abfdce0367abb519b
Zen <michaela.salamero@gmail.com>
2025-05-07 21:59:45 +0800

    added write-cname plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { writeFileSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/zensalamero.github.io/',
+  base: '/zensalamero.github.io/', // this is needed only if you're deploying to GitHub Pages repo path
   build: {
     outDir: 'dist',
   },
-  plugins: [vue(), vueJsx(), ...(process.env.NODE_ENV === 'development' ? [vueDevTools()] : [])],
+  plugins: [
+    vue(),
+    vueJsx(),
+    ...(process.env.NODE_ENV === 'development' ? [vueDevTools()] : []),
+    {
+      name: 'write-cname',
+      closeBundle() {
+        writeFileSync('dist/CNAME', 'zense.dev')
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@': '/src',
