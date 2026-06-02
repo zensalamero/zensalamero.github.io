5ea98cd3eeadaac2f8007891a625d2dc8276528a
Zen <michaela.salamero@gmail.com>
2026-06-02 15:00:01 +0800

    idk

--- src/app/App.vue
+++ src/app/App.vue
@@ -39,7 +39,7 @@ onMounted(() => {
 
 <template>
   <!-- Loading Screen with fade out transition -->
-  <Transition
+  <!-- <Transition
     name="loading-fade"
     enter-active-class="transition-opacity duration-700 ease-out"
     leave-active-class="transition-opacity duration-1000 ease-in-out"
@@ -51,10 +51,10 @@ onMounted(() => {
     <div v-if="isLoading" class="fixed inset-0 bg-background flex items-center justify-center z-50">
       <LoadingScreen />
     </div>
-  </Transition>
+  </Transition> -->
 
   <!-- Main Application with smooth fade in -->
-  <Transition
+  <!-- <Transition
     name="app-fade"
     enter-active-class="transition-all duration-1200 ease-out"
     leave-active-class="transition-all duration-500 ease-in"
@@ -62,16 +62,16 @@ onMounted(() => {
     enter-to-class="opacity-100 transform translate-y-0 scale-100"
     leave-from-class="opacity-100 transform translate-y-0 scale-100"
     leave-to-class="opacity-0 transform translate-y-4 scale-98"
-  >
-    <div v-if="showMainApp" class="min-h-screen">
-      <FileExplorer>
-        <LayoutWrapper>
-          <AppNavigation />
-          <component :is="currentView" />
-        </LayoutWrapper>
-      </FileExplorer>
-    </div>
-  </Transition>
+  > -->
+  <div v-if="showMainApp" class="min-h-screen sm:wd-full">
+    <FileExplorer>
+      <LayoutWrapper>
+        <AppNavigation />
+        <component :is="currentView" />
+      </LayoutWrapper>
+    </FileExplorer>
+  </div>
+  <!-- </Transition> -->
 
   <!-- Meta tags manager -->
   <MetaManager />
--- src/components/ui/LayoutWrapper.vue
+++ src/components/ui/LayoutWrapper.vue
@@ -1,6 +1,6 @@
 <!-- src/components/layout/LayoutWrapper.vue -->
 <template>
-  <div class="min-h-screen w-full flex flex-col transition-colors bg-background">
+  <div class="min-h-screen w-full h-screen flex flex-col transition-colors bg-background">
     <slot />
   </div>
 </template>
